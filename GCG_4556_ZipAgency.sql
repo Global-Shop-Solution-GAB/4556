173b1257a8a03e259a0febe96b2c8a6c52fe8982
Service Web <sw@gssmail.com>
2024-03-12 11:09:05 -0500

    Commit changes in C:\Websites\Git\emarlow\4556\GCG_4556_ZipAgency.sql

---
+++ GCG_4556_ZipAgency.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_4556_ZipAgency
+ * ========================================== */
+CREATE TABLE "GCG_4556_ZipAgency"(
+ "AgencyID" CHAR(3),
+ "State" CHAR(2),
+ "ZipCode" CHAR(13),
+ "column0" CHAR(3),
+ "column1" CHAR(2),
+ "column2" CHAR(13));
+
+
